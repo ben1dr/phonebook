4a575c85e6425e52b05d2a925de47857cb2713b9
ben <benyamin.derakhshani@gmail.com>
2021-12-12 12:37:31 +0330

    Complete edit and delete function and optimize program based on functional proceeds

--- phonebook.py
+++ phonebook.py
@@ -1,7 +1,9 @@
 import json
 import uuid
 import re
-# file_name = 'phonebook.txt'
+import time
+import getpass
+
 filename = 'phonebook.json'
 
 def add_contact():
@@ -25,8 +27,9 @@ def add_contact():
         fp.write(pretty_file)    
     print('Contact added.')
 
+
 def search_contact():
-    input_str = input("Welcome to search procedure. Please search by name or family or number:\n")
+    input_str = input("Welcome to search procedure. Please search by name or family or number: ")
     with open(filename, 'r') as f:
         fp = f.read()
         try:
@@ -36,78 +39,141 @@ def search_contact():
             print("No contact exists in phonebook. Maybe deleted or not added yet.")
             return
         
-        max_width = 12
         found_contact = []
-        for k_v in phonebook.values():
-            for value in k_v.values():
-                founded = re.search(f"^{input_str}$", value)
+        for key, value in phonebook.items():
+            for item in value.values():
+                founded = re.search(f"^{input_str}$", item)
                 if founded != None:
-                    found_contact.append(k_v)
-                    max_width = max(len(value), max_width)
-
-        print(f"Founded {len(found_contact)} number{'s' if len(found_contact)>1 else ''}:")
-        # if max_width > 11:
-        #     width_space = " "*(max_width - 11)
-        # else:
-        #     width_space = " "
-        # width_space = 14
-        if len(found_contact) > 0:
-            print(f"first name{4*' '} | last name{5*' '} | phone number{2*' '}")
+                    found_contact.append(key)
+        return found_contact
+ 
+    
+def show_contacts(id_list):
+    print(f"Founded {len(id_list)} number{'s' if len(id_list)>1 else ''}.")
+    if len(id_list) > 0:
+        number_mapped = {}
+        with open(filename, 'r') as f:
+            fp = f.read()
+            # try:
+            file = json.loads(fp)
+            phonebook = file
+            # except:
+            #     print("No contact exists in phonebook. Maybe deleted or not added yet.")
+            #     return
+            print(f" | first name{4*' '} | last name{5*' '} | phone number{2*' '}")
+        
+            for item in range(len(id_list)):
+                id_number = id_list[item] 
+                record = phonebook[id_number]
+                print("{:<1}. {:<14} | {:<14} | {:<14}".format(item+1,record['first_name'], record['last_name'], record['phone_number']))
+                number_mapped[item+1] = id_number
+        return number_mapped    
+        
+    else:
+        print('No contacts founded.')
+        return False
             
-            for record in found_contact:
-                print("{:<14} | {:<14} | {:<14}".format(record['first_name'], record['last_name'], record['phone_number']))
-                    # print(f"{record['first_name']}{lack_of_space} | " + 
-                    #     f"{record['last_name']}{lack_of_space} | "+
-                    #     f"{record['phone_number']}{lack_of_space}")
-                    # for cell in record.values():
-                    #     print(f"{0}{width_space} | {1}{width_space} | {2}{width_space}")
-                # print("".join(found))
-                
-        # print(file)
-        # found = []
-        # for i in file:
-        #     founded = re.search(f'r"\b{input_str}\b"', i)
-        #     if founded != None:
-        #         found.append(i)
-        # if len(found) == 0:     
-        #     print("No items founded.")
-        # else:
-        #     print(f"Founded {len(found)} number{'s' if len(found)>1 else ''}:")
-        #     print("".join(found))
-
-def delete_contact():
+        
+def authorization():
     admin = {'username': 'admin',
              'password': 'admin'}
-    "You choose delete contact option. Need approve your access level."
+    print("'This section need access level approval.'")
     user = input('Please enter username: ')
-    password = input("Please enter psasword: ")
+    password = getpass.getpass("Please enter psasword: ")
     if user == admin['username']:
         if password == admin['password']:
             print('Your access level approved.')
+            return True
     else:
         print('username or password incorrect. Access denied.')
-        return
-    search_contact()
+        return False
     
     
-        
-print('-*-*-*-"Welcome to terminal phonebook app."-*-*-*-')
-while True:
-    command = input('To add phone contact press "a", to delete press "d", ' + 
-                    'to search press "s", and to quit press "q" then press key "Enter".\n')
+def edit_contact(decide):
+    check = authorization()
+    if check:
+        items = search_contact()
+        contacts = show_contacts(items)
+        if contacts != False:
+            try:
+                if decide == 'delete':
+                    message = 'Please type contact row number that you want to delete it: '
+                elif decide == 'edit':
+                    message = 'Please type contact row number that you want to edit it: '
+                contact_chosen = int(input(message))
+                if 0 < contact_chosen <= len(contacts):
+                    with open(filename, 'r') as f:
+                        fp = f.read()
+                        file = json.loads(fp)
+                        phonebook = file
+                    
+                    
+                    if decide == 'delete':
+                        del phonebook[contacts[contact_chosen]]   
+                        print('Contact deleted successfully') 
+                    elif decide == 'edit':
+                        intended_contact = phonebook[contacts[contact_chosen]]
+                        print("If you don't want edit any field press enter key.")
+                        for key in intended_contact:
+                            input_value = input(f"Enter new value for {key}: ")
+                            if input_value:
+                                intended_contact[key] = input_value 
+                        phonebook[contacts[contact_chosen]] = intended_contact
+                        print('Contact edited successfully.')
 
-    if command == 'q':
-        break
-    elif command == "a":
-        add_contact()
-    elif command == 'd':
-        delete_contact()
-    elif command == 's':
-        search_contact()
-    else:
-        print('Your input command is wrong, press the correct key.')
-    print()
+                    pretty_file = json.dumps(phonebook, indent=4)
+                    with open(filename, 'w') as fp:
+                        fp.write(pretty_file)
+                else:
+                    print("Your input is wrong.")
+            except:
+                print('Your input is so wrong.')   
+            
+
+           
+    
 
     
+def menu(close):        
+    while True:
+        print("""Type your desired option number from menu, then press Enter key.
+    1. Add contact to phonebook
+    2. Search for contact(s)
+    3. Edit contact
+    4. Delete contact
+    5. Quit from app""")
+        try:
+            command = int(input('Your choice: '))
+            if command == 1:
+                add_contact()
+            elif command == 2:
+                output = search_contact()
+                show_contacts(output)
+            elif command == 3:
+                edit_contact(decide='edit')
+            elif command == 4:
+                edit_contact(decide='delete')
+            elif command == 5:
+                close = True
+        except:
+            print('You must type a number between 1 and 5. Please try again.')
+            time.sleep(.5)
+        finally:    
+            if close == True:
+                quit("""
+                Thank you for using my program.
+                Yours sincerely, 
+                        --> Benyamin Derakhshani <--
+                S e e   Y o u   L a t e r
+                     """)
+            input('-->Press Enter to continue...')
+            print()
+
+
+if __name__ == '__main__' :
+    print('-*-*-*-"Welcome to terminal phonebook app."-*-*-*-')
+    close = False     
+    menu(close)
+
         
         
